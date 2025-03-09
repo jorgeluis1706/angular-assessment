6274801ef7ac3df5ecca97e6199c853b9b39f937
Jorge Segura <jorge_segura@claro.com.do>
2025-03-09 19:23:56 -0400

    Improve app component spec test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import {LayoutComponent} from './shared/components/layout/layout.component';
+import {ListingComponent} from './modules/listing/listing.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [
+    LayoutComponent,
+    ListingComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-assessment';
+
 }
